7c657fd8de91d835fbcd58138eff8f12200e2940
VitorHSilver <vitor-henrique1996@hotmail.com>
2024-12-13 23:02:35 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+     content: ['./src/**/*.{html,js}', './FrontEndApp/src/**/*.{vue,js,ts,jsx,tsx}'],
+     theme: {
+          extend: {},
+     },
+     plugins: [],
+};
